a3358807ce3106c5bee868bf226bd1ce63b573ae
ARTEM POBEHAILO <partemishe@gmail.com>
2025-12-18 18:53:25 +0200

    form action

--- src/components/Form/Form.tsx
+++ src/components/Form/Form.tsx
@@ -1,23 +1,38 @@
 export default function Forms() {
-  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
-    const form = event.currentTarget;
-    const formData = new FormData(form);
-    const username = formData.get("username");
-    const gdfgdfg = formData.get("gdfgdfg");
-    const sasa = formData.get("sasa");
+  //   const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+  //     event.preventDefault();
+  //     const form = event.currentTarget;
+  //     const formData = new FormData(form);
+  //   const username = formData.get("username");
+  //   const gdfgdfg = formData.get("gdfgdfg");
+  //   const sasa = formData.get("sasa");
+  //   console.log("usr", username);
+  //   console.log("gdfgdfg", gdfgdfg);
+  //   console.log("sasa", sasa);
+
+  //     form.reset();
+
+  const handleSubmit = (formData: FormData) => {
+    const username = formData.get("username") as string;
+    const gdfgdfg = formData.get("gdfgdfg") as string;
+    const sasa = formData.get("sasa") as string;
     console.log("usr", username);
     console.log("gdfgdfg", gdfgdfg);
     console.log("sasa", sasa);
-
-    form.reset();
+    console.log("form submit");
   };
 
   return (
-    <form onSubmit={handleSubmit}>
-      <input type="text" name="username" />
-      <input type="text" name="gdfgdfg" />
-      <input type="text" name="sasa" />
+    // <form onSubmit={handleSubmit}>
+    //   <input type="text" name="username" />
+    //   <input type="text" name="gdfgdfg" />
+    //   <input type="text" name="sasa" />
+    //   <button type="submit">Submit</button>
+    //   </form>
+    <form action={handleSubmit}>
+      <input type="text" name="username" defaultValue="John Doe" />
+      <input type="text" name="gdfgdfg" defaultValue="John Doe" />
+      <input type="text" name="sasa" defaultValue="John Doe" />
       <button type="submit">Submit</button>
     </form>
   );
